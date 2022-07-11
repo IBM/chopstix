d66ef2fde296c29f55767a386727e6e8e360f2a8
Ramon Bertran Monfort <rbertra@us.ibm.com>
2022-07-11 15:32:04 -0400

    fix
    Signed-off-by: Ramon Bertran Monfort <rbertra@us.ibm.com>

--- tools/chop-marks-dyn-addr-lib.c
+++ tools/chop-marks-dyn-addr-lib.c
@@ -18,21 +18,58 @@
 #
 # ----------------------------------------------------------------------------
 #
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <string.h>
+#include <link.h>
 
 static void check_address() __attribute__((constructor));
 
-void check_address() {
+static int callback(struct dl_phdr_info *info, size_t size, void *data)
+{
+    char *type;
+    int p_type, j;
+
+    printf("Name: \"%s\" (%d segments)\n", info->dlpi_name,
+            info->dlpi_phnum);
+
+    for (j = 0; j < info->dlpi_phnum; j++) {
+        p_type = info->dlpi_phdr[j].p_type;
+        type =  (p_type == PT_LOAD) ? "PT_LOAD" :
+            (p_type == PT_DYNAMIC) ? "PT_DYNAMIC" :
+            (p_type == PT_INTERP) ? "PT_INTERP" :
+            (p_type == PT_NOTE) ? "PT_NOTE" :
+            (p_type == PT_INTERP) ? "PT_INTERP" :
+            (p_type == PT_PHDR) ? "PT_PHDR" :
+            (p_type == PT_TLS) ? "PT_TLS" :
+            (p_type == PT_GNU_EH_FRAME) ? "PT_GNU_EH_FRAME" :
+            (p_type == PT_GNU_STACK) ? "PT_GNU_STACK" :
+            (p_type == PT_GNU_RELRO) ? "PT_GNU_RELRO" : NULL;
+
+        printf("    %2d: [%14p; memsz:%7lx] flags: 0x%x; ", j,
+                (void *) (info->dlpi_addr + info->dlpi_phdr[j].p_vaddr),
+                info->dlpi_phdr[j].p_memsz,
+                info->dlpi_phdr[j].p_flags);
+        if (type != NULL)
+            printf("%s\n", type);
+        else
+            printf("[other (0x%x)]\n", p_type);
+    }
+
+    return 0;
+}
+
+static void check_address() {
+
+    dl_iterate_phdr(callback, NULL);
 
     char * symbol = getenv("LD_SYMBOL");
     if (symbol == NULL) {
         fprintf(stderr, "chop-marks-dyn-addr: not symbol specified in LD_SYMBOL");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     fprintf(stderr, "chop-marks-dyn-addr: search symbol: %s\n", symbol);
@@ -41,7 +78,7 @@ void check_address() {
     if (handle == NULL) {
         char * err = dlerror();
         fprintf(stderr, "chop-marks-dyn-addr: dlopen error: %s\n", err);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     void * addr = dlsym(handle, symbol);
@@ -49,10 +86,10 @@ void check_address() {
         char * err = dlerror();
         fprintf(stderr, "chop-marks-dyn-addr: dlsym error: %s\n", err);
         fprintf(stderr, "chop-marks-dyn-addr: Symbol not exported. Make sure it is defined using 'readelf -s <binary>'\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     fprintf(stderr, "chop-marks-dyn-addr: %s addr is: %p\n", symbol, addr);
     fprintf(stdout, "%p\n", addr);
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
