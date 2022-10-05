3906f824fcfc101890f3979c27e9f32a4f69864b
Ramon Bertran Monfort <rbertra@us.ibm.com>
2022-10-05 13:35:57 -0400

    Add API to wait child process to hit the main module after exec
    Signed-off-by: Ramon Bertran Monfort <rbertra@us.ibm.com>

--- src/core/process.h
+++ src/core/process.h
@@ -83,6 +83,7 @@ class Process {
     void detach(int sig = 0);
     void step(int sig = 0);
     void steps(long steps);
+    void step_to_main_module();
 
     long peek(long addr);
     void poke(long addr, long data);
@@ -121,5 +122,6 @@ class Process {
     int status_;
 
     breakpoint_cache breaks_;
+    char* mainmodule_;
 };
 }  // namespace chopstix
